354b79140d25cad6009aadb4f4cdd6c34bde9033
dionisos <denis.baudouin@gmail.com>
2013-03-15 15:31:15 +0100

    try to make composer work

--- Tests/Service/AutoloadEnum.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Ukratio\ToolBundle\Tests\Service;
-
-use Ukratio\ToolBundle\Service\Enum;
-
-Enum::enum('AutoloadEnum', array('a1', 'a2', 'a3'), 'Ukratio\ToolBundle\Tests\Service');
